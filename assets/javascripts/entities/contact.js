2b945113e0c4092fe38909363eed3d44e7779372
Sasha Egorov <a.a.egoroff@gmail.com>
2013-12-15 02:41:07 +0400

    More modularization

--- assets/javascripts/entities/contact.js
+++ assets/javascripts/entities/contact.js
@@ -20,4 +20,29 @@ ContactManager.module('Entities', function(
       return full_name;
     }
   });
+
+  var contacts;
+  var initializeContacts = function(){
+    contacts = new Entities.ContactCollection([
+      { id: '1', firstName: 'Alice',   lastName: 'Tampen',   phoneNumber: '555-0001' },
+      { id: '2', firstName: 'Bob',     lastName: 'Brigham',  phoneNumber: '555-0002' },
+      { id: '3', firstName: 'Alice',   lastName: 'Artsy',    phoneNumber: '555-0003' },
+      { id: '4', firstName: 'Alice',   lastName: 'Arten',    phoneNumber: '555-0004' },
+      { id: '5', firstName: 'Charlie', lastName: 'Campbell', phoneNumber: '555-0005' },
+      { id: '6', firstName: 'Alice',   lastName: 'Smith',    phoneNumber: '555-0006' }
+    ]);
+  }
+  
+  var API = {
+    getContactsEntities: function () {
+      if (contacts === undefined) {
+        initializeContacts();
+      }
+      return  contacts;
+    }
+  }
+  
+  ContactManager.reqres.setHandler('contact:entities', function(){
+    return API.getContactsEntities();
+  })
 });
