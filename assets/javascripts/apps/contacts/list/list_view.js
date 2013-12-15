17db446ce2aff38e6788ea585cbdae9d2c0364cc
Sasha Egorov <a.a.egoroff@gmail.com>
2013-12-15 23:12:51 +0400

    Alert class added

--- assets/javascripts/apps/contacts/list/list_view.js
+++ assets/javascripts/apps/contacts/list/list_view.js
@@ -1,7 +1,15 @@
 ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbone, Marionette, $, _){
   List.Contact = Marionette.ItemView.extend({
     tagName: 'tr',
-    template: List.Templates.listItemView
+    template: List.Templates.listItemView,
+    events: {
+      'click': 'highlightNames'
+    },
+    highlightNames: function(){
+      console.log(this);
+      this.$el.toggleClass('alert');
+    }
+
   });
   List.Contacts = Marionette.CompositeView.extend({
     tagName: 'table',
