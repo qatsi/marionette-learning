eeaa2c4a016881833b12fb75f954cffabc7caee8
Sasha Egorov <a.a.egoroff@gmail.com>
2013-12-16 00:33:15 +0400

    Displaying the Contents of a Clicked Table Cell

--- assets/javascripts/apps/contacts/list/list_view.js
+++ assets/javascripts/apps/contacts/list/list_view.js
@@ -3,13 +3,20 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
     tagName: 'tr',
     template: List.Templates.listItemView,
     events: {
-      'click': 'highlightNames'
+      // 'click': 'highlightNames',
+      'click td': 'showInfo', 
     },
     highlightNames: function(e){
       // console.log(this);
       e.preventDefault();
       this.$el.toggleClass('alert');
+    },
+    showInfo: function(e){
+      console.log(e);
+      e.preventDefault();
+      alert($(e.target).text());
     }
+    
 
   });
   List.Contacts = Marionette.CompositeView.extend({
