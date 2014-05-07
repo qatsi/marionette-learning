97354344fd78919c65f99fc4163f0cb9b0a5b12c
Sasha Egorov <a.a.egoroff@gmail.com>
2014-05-07 19:44:00 +0400

    Routing refactored (p.80/92)

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -4,11 +4,20 @@ ContactManager.addRegions({
   mainRegion: "#main-region"
 });
 
+ContactManager.navigate = function(route, options){
+  options || (options = {});
+  Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function(){
+  return Backbone.history.fragment;
+}
+
 ContactManager.on('initialize:after', function(){
   if (Backbone.history) {
     Backbone.history.start();
-    if (Backbone.history.fragment === '') {
-      Backbone.history.navigate('contacts');
+    if (this.getCurrentRoute() === '') {
+      this.navigate('contacts');
       // Call it directly. Don't use {trigger: true}
       ContactManager.ContactsApp.List.Controller.listContacts();
     }
